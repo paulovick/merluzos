dd70bf84de536b1dafe0d554215e490414215133
Pau Torrents <pautg.95@gmail.com>
2018-10-17 14:14:00 +0200

    Safe commit

--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class App extends React.Component {
   render() {
+    const { containerStyle, textContainerStyle, mapStyle } = styles
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={containerStyle}>
+        <View style={textContainerStyle}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Text 3</Text>
+        </View>
+
+        <View style={mapStyle}>
+          {/* <MapView
+            initialRegion={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          /> */}
+          <Text>New text</Text>
+        </View>
       </View>
     )
   }
 }
 
 const styles = {
-  container: {
+  containerStyle: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+  },
+  textContainerStyle: {
+    
+  },
+  mapStyle: {
+    backgroundColor: '#f00'
   }
 }
