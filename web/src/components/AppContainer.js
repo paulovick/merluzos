7dd32b98b41fece9cdbe41ddf9f2bce2cefba23c
Pau Torrents <pautg.95@gmail.com>
2018-10-17 16:50:57 +0200

    Added Map to AppContainer

--- web/src/components/AppContainer.js
+++ web/src/components/AppContainer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import MapContainer from './MapContainer'
 
 class AppContainer extends Component {
   render() {
     return (
       <div>
-        <p>Apppp</p>
+        <MapContainer/>
       </div>
     )
   }
