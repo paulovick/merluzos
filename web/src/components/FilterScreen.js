79ed2ad7c530d4ae3a369a16fa62038454374322
Pau Torrents <pautg.95@gmail.com>
2018-10-17 22:00:17 +0200

    Centered

--- web/src/components/FilterScreen.js
+++ web/src/components/FilterScreen.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+
 class FilterScreen extends Component {
   render() {
-    const { containerStyle } = styles
+    const { containerStyle, outerContainerStyle, middleContainerStyle, innerContainerStyle } = styles
 
     return (
       <div style={containerStyle}>
-        <p>Filter screen</p>
+        <div style={outerContainerStyle}>
+          <div style={middleContainerStyle}>
+            <div style={innerContainerStyle}>
+              <p>Centrered</p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
@@ -15,7 +22,32 @@ class FilterScreen extends Component {
 
 const styles = {
   containerStyle: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
     backgroundColor: 'white'
+  },
+  outerContainerStyle: {
+    position: 'absolute',
+    display: 'table',
+    height: '100%',
+    width: '100%'
+  },
+  middleContainerStyle: {
+    display: 'table-cell',
+    verticalAlign: 'middle'
+  },
+  innerContainerStyle: {
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 'auto',
+    marginBottom: 'auto',
+    textAlign: 'center',
+    backgroundColor: 'red',
+    width: '80%'
   }
 }
 
