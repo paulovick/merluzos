91b1d55037f1ba14bc9a301e99f576c393b58585
Pau Torrents <pautg.95@gmail.com>
2018-10-17 16:15:35 +0200

    Refactor

---
+++ web/src/reducers/filterReducer.js
@@ -0,0 +1,14 @@
+
+const INITIAL_STATE = {
+  fromText: '',
+  toText: '',
+  fromPosition: null,
+  toPosition: null
+}
+
+const filterReducer = (state = INITIAL_STATE, action) => {
+  // TODO
+  return state
+}
+
+export { filterReducer }
