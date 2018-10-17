f6e4865f7817a3002b917391139f21c221b8395a
Josefran <josefran@fib.upc.edu>
2018-10-17 17:35:39 +0200

    Routing Service return 2 routes (eco and no eco)

--- web/src/routing.js
+++ web/src/routing.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 
 class Point {
@@ -10,37 +10,36 @@ class Point {
 }
 
 class Route {
-    constructor(from, to, transport) {
-        this.from = from;
-        this.to= to;
-        this.transport = transport;
+    constructor(routeJson) {
+        this.airQuality = routeJson.airQualityRating;
+        this.time = routeJson.travelTime;
+        this.eco = routeJson.eco;
+        this.distance = routeJson.distance;
+        this.points = this.buildPoints(routeJson.route);
     }
+
+    buildPoints(route) {
+        let result = [];
+        route.forEach(function (point) {
+            let p = {
+                lat: point.location.latitude,
+                long: point.location.longitude
+            };
+            result.push(p);
+        });
+        return result;
+    }
+
+    getPointsToPaint() {
+        return this.points;
+    }
+
+
 }
 
+class RoutingService {
 
-class RoutingService extends Component {
-    constructor(props) {
-        super(props);
-
-        this.getRoute = this.getRoute.bind(this);
-        this.buildRouteUrl = this.buildRouteUrl.bind(this);
-        this.buildFakeRouteUrl = this.buildFakeRouteUrl.bind(this);
-        this.checkStatus = this.checkStatus.bind(this);
-    }
-
-    componentWillMount() {
-        let from = new Point(16.3657665, 48.2114620);
-        let to = new Point(16.18465, 48.216799);
-        let transport = 'foot';
-        this.getRoute(from, to, transport)
-    }
-
-    checkStatus(res) {
-        if (res.ok) { // res.status >= 200 && res.status < 300
-            return res;
-        } else {
-            throw Error(res.statusText);
-        }
+    constructor() {
     }
 
     buildRouteUrl(from, to, transport) {
@@ -57,14 +56,48 @@ class RoutingService extends Component {
         return 'http://localhost:3000/route.json'
     }
 
-    getRoute(from, to, transport) {
+    getRoutes(from, to, transport) {
         const url = this.buildFakeRouteUrl(from, to, transport);
-        fetch(url)
+        return fetch(url)
             .then(this.checkStatus)
             .then(res => res.json())
-            .then(json => console.log(json))
+            .then(json => {
+                let result = [];
+                json.forEach(function (routeJson) {
+                    let route = new Route(routeJson);
+                    result.push(route);
+                });
+                return result;
+            })
             .catch(err => console.error(err));
     }
+
+}
+
+class RoutingComponent extends Component {
+    constructor(props) {
+        super(props);
+
+        this.checkStatus = this.checkStatus.bind(this);
+    }
+
+    componentWillMount() {
+        let from = new Point(16.3657665, 48.2114620);
+        let to = new Point(16.18465, 48.216799);
+        let transport = 'foot';
+        new RoutingService().getRoutes(from, to, transport).then(routes => {
+            console.log(routes);
+        });
+    }
+
+    checkStatus(res) {
+        if (res.ok) { // res.status >= 200 && res.status < 300
+            return res;
+        } else {
+            throw Error(res.statusText);
+        }
+    }
+
 
     render() {
         return (
@@ -74,4 +107,4 @@ class RoutingService extends Component {
 }
 
 
-export default RoutingService
+export default RoutingComponent
